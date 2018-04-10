e02d9c10429367be882bbac338ea99a1a2d1972b
jiehao <434471446@qq.com>
2018-04-10 09:02:11 +0800

    开坑声明codewars

---
+++ src/pratice/Introduction/Problem6174.java
@@ -0,0 +1,26 @@
+package pratice.Introduction;
+
+import java.util.Arrays;
+
+public class Problem6174 {
+    public void solve6174(String start)
+    {
+        char[] sortChar = start.toCharArray();
+        Arrays.sort(sortChar);
+        String sortStr = new StringBuilder(String.valueOf(sortChar)).reverse().toString();
+        String nextStr = String.valueOf(Math.abs(Integer.parseInt(sortStr) - Integer.parseInt(start)));
+        while (!nextStr.equals(start))
+        {
+            start = nextStr;
+            System.out.println(nextStr);
+            sortChar = start.toCharArray();
+            Arrays.sort(sortChar);
+            sortStr = new StringBuilder(String.valueOf(sortChar)).reverse().toString();
+            nextStr = String.valueOf(Math.abs(Integer.parseInt(sortStr) - Integer.parseInt(start)));
+        }
+    }
+    public void test()
+    {
+        solve6174("1234");
+    }
+}
