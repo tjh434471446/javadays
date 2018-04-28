52b5a437a9a6bacf0e65db8fb941a9a9fdd7195e
jiehao <434471446@qq.com>
2018-04-28 18:30:06 +0800

    添加junit

--- src/main/java/com/jiehao/util/ElasticsearchUtil.java
+++ src/main/java/com/jiehao/util/ElasticsearchUtil.java
@@ -1,5 +1,5 @@
 package com.jiehao.util;
-
+/*
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.TransportAddress;
@@ -7,15 +7,17 @@ import org.elasticsearch.transport.client.PreBuiltTransportClient;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+*/
 
 public class ElasticsearchUtil {
     public String test(){
+        /*
         try {
             TransportClient client = new PreBuiltTransportClient(Settings.EMPTY)
                     .addTransportAddress(new TransportAddress(InetAddress.getByName("host1"),9200));
         } catch (UnknownHostException e) {
             e.printStackTrace();
-        }
+        }*/
         return "";
     }
 }
